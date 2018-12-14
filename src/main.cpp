058a8422331ecef78fa4b4d5643059abe00181b2
Parker Lusk <parkerclusk@gmail.com>
2018-12-14 13:37:00 -0500

    cool colors

--- src/main.cpp
+++ src/main.cpp
@@ -42,24 +42,49 @@ int main(int argc, char const *argv[])
 
   std::cout << "Detected " << corners.size() << " features in " << t_detect.toc() << " ms." << std::endl;
 
+  //
   // Score statistics
+  // 
+
   float maxScore = *std::max_element(scores.begin(), scores.end());
-  std::cout << "Maximum feature score: " << maxScore << std::endl;
+  float minScore = *std::min_element(scores.begin(), scores.end());
+  std::cout << "Feature score range: [" << minScore << ", " << maxScore << "]" << std::endl;
 
-  // draw features
-  for (size_t i=0; i<corners.size(); ++i) {
-    // convenience
-    auto pt = corners[i];
-    auto score = scores[i] / maxScore;
+  // the score normalizer
+  int n = 0;
 
-    // color alpha mixing
-    auto color = cv::Scalar(0, 255*score, 255*(1 - score));
-    cv::circle(img, pt, 2, color, 2);
-  }
-
-  cv::imshow("Detections", img);
   while(1) {
+
+    cv::Mat draw = img.clone();
+
+    // get nth element to be used as normalizer
+    auto s = scores;
+    std::nth_element(s.begin(), s.begin()+n, s.end(), std::greater<float>());
+    float eta = s[n];
+    std::cout << n+1 << "-th greatest score: " << eta << std::endl;
+
+    // draw features
+    for (size_t i=0; i<corners.size(); ++i) {
+      // convenience
+      auto pt = corners[i];
+      float score = std::min(1.0f, static_cast<float>(scores[i] / eta));
+
+      // color alpha mixing
+      auto color = cv::Scalar(0, 255*score, 255*(1 - score));
+      cv::circle(draw, pt, 2, color, 2);
+
+      // print normalized score next to feature
+      // char strScore[10];
+      // snprintf(strScore, sizeof(strScore), "%0.2f", score);
+      // cv::putText(draw, strScore, pt, cv::FONT_HERSHEY_SIMPLEX, 0.3, cv::Scalar(200, 0, 0));
+    }
+
+    cv::imshow("Detections", draw);
     char key = static_cast<char>(cv::waitKey(0));
+
+    if (key == '+' && n<corners.size()) n++;
+    if (key == '-' && n>0) n--;
+
     if (key == static_cast<char>(27)) break; // esc
   }
 }
